1ee9d12473764ac45e226d3784fab52c9559225d
adrianmendozatabaz <myamyamya29@gmail.com>
2025-10-27 20:46:49 -0600

    Project create with Vue Js, Boostrap and LocalStorage

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<!-- //* Single file conponents -->
+
+<!-- //* Logica de los componentes -->
+<script setup>
+import { ref, reactive, onMounted, watch } from 'vue'
+import { db } from './data/guitars'
+import Guitarra from './components/Guitarra.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+
+const guitarras = ref([])
+const carrito = ref([])
+const guitarra = ref({})
+
+watch(carrito, () => {
+    guardarLocalStorage();
+}, {
+    deep: true
+})
+
+onMounted(() => {
+    guitarras.value = db
+    guitarra.value = db[3]
+
+    const carritoStorage = localStorage.getItem('carrito')
+    if (carritoStorage) {
+        carrito.value = JSON.parse(carritoStorage)
+    }
+})
+
+const guardarLocalStorage = () => {
+    localStorage.setItem('carrito', JSON.stringify(carrito.value))
+}
+
+const agregarCarrito = (guitarra) => {
+    const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+    if (existeCarrito >= 0) {
+        carrito.value[existeCarrito].cantidad++
+    } else {
+        carrito.value.push(guitarra)
+        guitarra.cantidad = 1
+    }
+}
+
+const decrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if(carrito.value[index].cantidad <= 1) return
+    carrito.value[index].cantidad--
+}
+
+const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if(carrito.value[index].cantidad >= 5) return
+    carrito.value[index].cantidad++
+}
+
+const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter(producto => producto.id !== id)
+}
+
+const vaciarCarrito = () => {
+    carrito.value = []
+}
+
+</script>
+
+<!-- //* Toda la presentacion html  -->
+<template>
+
+    <Header 
+        :carrito="carrito"
+        :guitarra="guitarra"
+        @incrementar-cantidad="incrementarCantidad" 
+        @decrementar-cantidad="decrementarCantidad"
+        @agregar-carrito="agregarCarrito"
+        @eliminar-producto="eliminarProducto"
+        @vaciar-carrito="vaciarCarrito"
+    />
+
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
+
+        <div class="row mt-5">
+            <Guitarra v-for="guitarra in guitarras" :guitarra="guitarra" @agregar-carrito=agregarCarrito />
+        </div>
+    </main>
+
+    <Footer />
+</template>
+
+<!-- //* CSS, SCSS como tal o sass -->
+<style scoped></style>
